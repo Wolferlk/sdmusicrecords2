d14fae672ba054d2fc632da7bf00e061dc1eaf48
wolferlk <sasiofficial25@gmail.com>
2024-11-15 15:56:50 +0530

    front

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -117,5 +117,59 @@ export const artistsData = [
         year: '2023'
       }
     ]
+  },{
+    id: 'luna-eclipse',
+    name: 'We Are Brothers',
+    genre: 'Pop , Accoustic',
+    spot: 'https://open.spotify.com/embed/artist/5ajmxFKVTfNM0FZYRPCiko?utm_source=generator&theme=0',
+    yts:'https://www.youtube.com/embed/0WbwxTb1i8s?si=4_A135D7qmMq3Gkm',
+    image: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/455042860_381361151649254_7132611151953735098_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEqfpLreXfvF-2ANTi8itVrUx9xNGvmezZTH3E0a-Z7NvaaqUcx2iFQ8i8b7PZsWxPZFB30LooDbpV6TFizxmOn&_nc_ohc=SyJtl8a6dlQQ7kNvgHi8H78&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=A6jiavR9ai_yI2mdWSjeZYn&oh=00_AYBUpEJ3qVu5frU4O_elUhJjnRhrh8b5gQRLUnzmFBGa0A&oe=673CA76E',
+    coverImage: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/352184874_723913449488619_3752084457123172914_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeFgygLybCAuumBljme3MpI0VxQ9A8_tpytXFD0Dz-2nK2lJRSPb4AMVYmovBRf_06CkyR-lyBHLK04lJ-Eh2cvK&_nc_ohc=V4KeNKHQaA8Q7kNvgG9Dna2&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=A1gO5ZKSUUzJMFyX22vf9ys&oh=00_AYDIqTzUL3CkF8LGPC5sC3KRzrabqzf31n351QSGDdRuuQ&oe=673C80CC',
+    bio: 'We Are Brothers" is a talented music artist team based in Sri Lanka, dedicated to producing remarkable cover songs with a vision to venture into original compositions in the future. The group comprises gifted individuals including Amalsha Sapumal, Oshen Walimuni, Vishen Sanjana, Buwneka Hasaranga, Kavindu Nipun, Yehan Yugantha, Sasindu Diluranga ,Pasindu Hashan, Malindu Ishan, and Oshada Vihanga.As of 2023, "We Are Brothers" has reached a significant milestone by signing a contract with SD Music Records, a renowned music label, which is expected to provide them with valuable opportunities and exposure in the industry.Their harmonious blend of talents and creative abilities has earned them a strong following, and they continue to captivate audiences with their soulful renditions of popular songs. With the support of SD Music Records, they are excited to embark on a journey to create and release original music, aiming to further establish themselves as prominent figures in the Sri Lankan music scene.',
+    social: {
+      instagram: 'https://instagram.com/lunaeclipse',
+      spotify: 'https://open.spotify.com/artist/5ajmxFKVTfNM0FZYRPCiko?si=vEBrPHsYQZKt_yuk-YUHmg', 
+      twitter: 'https://twitter.com/lunaeclipse',
+      youtube: 'https://youtube.com/lunaeclipse'
+    },
+    website: 'https://lunaeclipse.com',
+    releases: [
+      {
+        id: 'neon-dreams',
+        title: 'Neon Dreams',
+        cover: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?auto=format&fit=crop&q=80',
+        year: '2023'
+      },
+      {
+        id: 'electric-soul',
+        title: 'Electric Soul',
+        cover: 'https://images.unsplash.com/photo-1504898770365-14faca6a7320?auto=format&fit=crop&q=80',
+        year: '2022'
+      }
+    ]
+  },
+  {
+    id: 'midnight-pulse',
+    name: 'Wolfer',
+    genre: 'FutureBase , House , Dance/Electronic',
+    spot:'https://open.spotify.com/embed/artist/1BDgRUInxjvI7BrgASGJUd?utm_source=generator'   ,
+    yts:'https://www.youtube.com/embed/w7dRjYu2iVE?rel=0&start=42',
+    image: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/272351746_1220431025032164_7851155204835571111_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHRy5O8GvCAUGQDZ30lMo1-_5gzQ4y_047_mDNDjL_Tjqkk8tiBNpJKuza_3uBgPEaPaCpqXPz8uvAFn_h9RvYi&_nc_ohc=9TrLgNHMDTQQ7kNvgEATgW1&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=A8_I1nKZ_WyOJnH-T90zENq&oh=00_AYCsYLZgY-YoKDOAcMktZHiCPWAun1H8tnzALQ7_O4SG6w&oe=673C9A3E',
+    coverImage: 'https://scontent.fcmb2-2.fna.fbcdn.net/v/t39.30808-6/291687303_1326990827709516_2483190681631040968_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeFSSC7T5aS3-RsdV__rzdpTiqjpFNyfKmCKqOkU3J8qYFOW5pZkNWezU0ciwB_SKtD93qa4bpmJuKEuz3XPsG9X&_nc_ohc=JaPB5ArYt3QQ7kNvgHBcPFw&_nc_zt=23&_nc_ht=scontent.fcmb2-2.fna&_nc_gid=AII00iXkc03izYcjU7sIUCE&oh=00_AYB6v_WQqDbPwzI-tlJllZDcAu8Aa7Ptk6-T8GxY9WWitA&oe=673CB7D5',
+    bio: "Meet Wolfer, the dynamic DJ/Producer, composer, and mentor from Sri Lanka. Born in 2001, his musical journey started at 14, fueled by an unyielding passion.Specializing in Dance/Electronic, Future Bass, House Music, and EDM, Wolfer collaborates with Sri Lanka's top artists. In 2023, he showcased his brilliance in the Rider film, directed by Rajith Hiran and produced by Anushka Idunil.As founder of SD MUSIC RECORDS and owner of a cutting-edge studio, Wolfer mentors emerging artists, sharing his wisdom. With a degree in Electronic Music Composition and advanced mixing/mastering, he collaborates with industry giants.In 2024, Wolfer's first live show for 3000 fans marked a milestone, showcasing his electrifying stage presence. This solidified his status as a force in electronic music. Embark on Wolfer's sonic journey, where beats tell stories and notes resonate with passion.",
+    social: {
+      instagram: 'https://instagram.com/midnightpulse',
+      twitter: 'https://twitter.com/midnightpulse',
+      youtube: 'https://youtube.com/midnightpulse'
+    },
+    website: 'https://midnightpulse.com',
+    releases: [
+      {
+        id: 'midnight-hour',
+        title: 'Midnight Hour',
+        cover: 'https://images.unsplash.com/photo-1571330735066-03aaa9429d89?auto=format&fit=crop&q=80',
+        year: '2023'
+      }
+    ]
   }
 ];
