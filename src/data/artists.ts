88299c2f97b5e2f22fb1e171f23f363edfdc5e03
wolferlk <sasiofficial25@gmail.com>
2025-01-10 14:43:24 +0530

    datarenew

--- src/data/artists.ts
+++ src/data/artists.ts
@@ -1,106 +1,96 @@
 export const artistsData = [
   {
-    id: 'luna-eclipse',
+    id: 'WeAreBrothers',
     name: 'We Are Brothers',
     genre: 'Pop , Accoustic',
     spot: 'https://open.spotify.com/embed/artist/5ajmxFKVTfNM0FZYRPCiko?utm_source=generator&theme=0',
     yts:'https://www.youtube.com/embed/0WbwxTb1i8s?si=4_A135D7qmMq3Gkm',
-    image: 'https://ibb.co/Xpyf22L',
-    coverImage: 'https://i.ibb.co/8BvkrbK/Weare.jpg',
+    image: 'https://i.ibb.co/YWngcXR/Weare.jpg',
+    coverImage: 'https://i.ibb.co/YWngcXR/Weare.jpg',
+    bio: 'We Are Brothers" is a talented music artist team based in Sri Lanka, dedicated to producing remarkable cover songs with a vision to venture into original compositions in the future. The group comprises gifted individuals including Amalsha Sapumal, Oshen Walimuni, Vishen Sanjana, Buwneka Hasaranga, Kavindu Nipun, Yehan Yugantha, Sasindu Diluranga ,Pasindu Hashan, Malindu Ishan, and Oshada Vihanga.As of 2023, "We Are Brothers" has reached a significant milestone by signing a contract with SD Music Records, a renowned music label, which is expected to provide them with valuable opportunities and exposure in the industry.Their harmonious blend of talents and creative abilities has earned them a strong following, and they continue to captivate audiences with their soulful renditions of popular songs. With the support of SD Music Records, they are excited to embark on a journey to create and release original music, aiming to further establish themselves as prominent figures in the Sri Lankan music scene.',
     social: {
       instagram: 'https://instagram.com/lunaeclipse',
       spotify: 'https://open.spotify.com/artist/5ajmxFKVTfNM0FZYRPCiko?si=vEBrPHsYQZKt_yuk-YUHmg', 
       twitter: 'https://twitter.com/lunaeclipse',
       youtube: 'https://youtube.com/lunaeclipse'
     },
-    website: 'https://lunaeclipse.com',
+    website: '',
     releases: [
-      {
-        id: 'neon-dreams',
-        title: 'Neon Dreams',
-        cover: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?auto=format&fit=crop&q=80',
-        year: '2023'
-      },
-      {
-        id: 'electric-soul',
-        title: 'Electric Soul',
-        cover: 'https://images.unsplash.com/photo-1504898770365-14faca6a7320?auto=format&fit=crop&q=80',
-        year: '2022'
-      }
+      
     ]
   },
   {
-    id: 'midnight-pulse',
+    id: 'Wolfer',
     name: 'Wolfer',
     genre: 'FutureBase , House , Dance/Electronic',
     spot:'https://open.spotify.com/embed/artist/1BDgRUInxjvI7BrgASGJUd?utm_source=generator'   ,
     yts:'https://www.youtube.com/embed/w7dRjYu2iVE?rel=0&start=42',
-    image: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/272351746_1220431025032164_7851155204835571111_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHRy5O8GvCAUGQDZ30lMo1-_5gzQ4y_047_mDNDjL_Tjqkk8tiBNpJKuza_3uBgPEaPaCpqXPz8uvAFn_h9RvYi&_nc_ohc=9TrLgNHMDTQQ7kNvgEATgW1&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=A8_I1nKZ_WyOJnH-T90zENq&oh=00_AYCsYLZgY-YoKDOAcMktZHiCPWAun1H8tnzALQ7_O4SG6w&oe=673C9A3E',
-    coverImage: 'https://scontent.fcmb2-2.fna.fbcdn.net/v/t39.30808-6/291687303_1326990827709516_2483190681631040968_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeFSSC7T5aS3-RsdV__rzdpTiqjpFNyfKmCKqOkU3J8qYFOW5pZkNWezU0ciwB_SKtD93qa4bpmJuKEuz3XPsG9X&_nc_ohc=JaPB5ArYt3QQ7kNvgHBcPFw&_nc_zt=23&_nc_ht=scontent.fcmb2-2.fna&_nc_gid=AII00iXkc03izYcjU7sIUCE&oh=00_AYB6v_WQqDbPwzI-tlJllZDcAu8Aa7Ptk6-T8GxY9WWitA&oe=673CB7D5',
+    image: 'https://i.ibb.co/M7CTKHH/Whats-App-Image-2024-10-07-at-9-58-55-PM-removebg-preview.png',
+    coverImage: 'https://i.ibb.co/LYkXk4n/spotify2-bs.jpg',
     bio: "Meet Wolfer, the dynamic DJ/Producer, composer, and mentor from Sri Lanka. Born in 2001, his musical journey started at 14, fueled by an unyielding passion.Specializing in Dance/Electronic, Future Bass, House Music, and EDM, Wolfer collaborates with Sri Lanka's top artists. In 2023, he showcased his brilliance in the Rider film, directed by Rajith Hiran and produced by Anushka Idunil.As founder of SD MUSIC RECORDS and owner of a cutting-edge studio, Wolfer mentors emerging artists, sharing his wisdom. With a degree in Electronic Music Composition and advanced mixing/mastering, he collaborates with industry giants.In 2024, Wolfer's first live show for 3000 fans marked a milestone, showcasing his electrifying stage presence. This solidified his status as a force in electronic music. Embark on Wolfer's sonic journey, where beats tell stories and notes resonate with passion.",
     social: {
-      instagram: 'https://instagram.com/midnightpulse',
-      twitter: 'https://twitter.com/midnightpulse',
-      youtube: 'https://youtube.com/midnightpulse'
+      instagram: 'https://www.instagram.com/wolfer_sl/',
+      twitter: 'https://www.facebook.com/SASINDU.A.DILURANGA',
+      youtube: 'https://www.youtube.com/@SASINDUDILURANGA'
     },
-    website: 'https://midnightpulse.com',
+    website: 'https://sasindudiluranga.vercel.app/',
     releases: [
-      {
-        id: 'midnight-hour',
-        title: 'Midnight Hour',
-        cover: 'https://images.unsplash.com/photo-1571330735066-03aaa9429d89?auto=format&fit=crop&q=80',
-        year: '2023'
-      }
+      // {
+      //   id: 'midnight-hour',
+      //   title: 'Midnight Hour',
+      //   cover: 'https://images.unsplash.com/photo-1571330735066-03aaa9429d89?auto=format&fit=crop&q=80',
+      //   year: '2023'
+      // }
     ]
   },
   {
-    id: 'echo-valley',
+    id: 'OshadaVihanga',
     name: 'Oshada Vihanga',
     genre: 'Indie Pop Classic',
-    image: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/463188694_122177339756167700_5318154937193409297_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG-xtp9XXPcisZ_46YeANzFpDstC0SJmxukOy0LRImbGwqRF3p_Qx_aaqMTnlLDvk-iTE3XA5kA2YkKBU6epUYR&_nc_ohc=4ojcGya9yqQQ7kNvgGe0J_h&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=AGqpVhlY0N3r8fV8J_9YCuq&oh=00_AYA7zZR0uJ7YeyI6youHKSVj7egTLG86MGMh_pZsvXkEPQ&oe=673C8AC4',
-    coverImage: 'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?auto=format&fit=crop&q=80',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCfc5NXpBtNIWl8RjysA2mTQPHZMxRQs3JzA&s',
+    coverImage: 'https://i.ytimg.com/vi/ijjEzJ4OdE4/hqdefault.jpg?v=63f0af26',
     bio: 'Echo Valley brings a fresh perspective to indie folk with their harmony-rich arrangements and storytelling approach. The duo met at a local coffee shop\'s open mic night and immediately connected over their shared love of folk music.\n\nTheir acoustic-driven sound and authentic lyrics have earned them spots at major folk festivals across the country. Their latest EP showcases their evolution as artists while staying true to their roots.',
     social: {
-      instagram: 'https://instagram.com/echovalley',
-      twitter: 'https://twitter.com/echovalley',
-      youtube: 'https://youtube.com/echovalley'
+      instagram: 'https://instagram.com/',
+      twitter: 'https://twitter.com/',
+      youtube: 'https://youtube.com/'
     },
     website: 'https://echovalley.com',
     releases: [
-      {
-        id: 'mountain-song',
-        title: 'Mountain Song',
-        cover: 'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?auto=format&fit=crop&q=80',
-        year: '2023'
-      }
+      // {
+      //   id: 'mountain-song',
+      //   title: 'Mountain Song',
+      //   cover: 'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?auto=format&fit=crop&q=80',
+      //   year: '2023'
+      // }
     ]
   },{
     id: 'Denethwiduranga',
-    name: 'Deneth Viduranga',
+    name: 'DenethViduranga',
     genre: 'Drum&Bass',
-    image: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t51.75761-15/466021815_18293478088229321_4523986414111524634_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFtPpfa7e0GF2j-89yWVzQEAY-AT5EpkgQBj4BPkSmSBJly0TbDz4reVVo271khX25ga6zciT9GaVk4XBYBqAmv&_nc_ohc=RbwNHZ1StWgQ7kNvgGLjJ3Q&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=AO2A101lCi904ofglyl7AsL&oh=00_AYDVl5tunTuNqVc8W7o05MaVFORFTxQmk21_FlBGUS8bAQ&oe=673C9A4F',
-    coverImage: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t51.75761-15/466021815_18293478088229321_4523986414111524634_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFtPpfa7e0GF2j-89yWVzQEAY-AT5EpkgQBj4BPkSmSBJly0TbDz4reVVo271khX25ga6zciT9GaVk4XBYBqAmv&_nc_ohc=RbwNHZ1StWgQ7kNvgGLjJ3Q&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=AO2A101lCi904ofglyl7AsL&oh=00_AYDVl5tunTuNqVc8W7o05MaVFORFTxQmk21_FlBGUS8bAQ&oe=673C9A4F',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfuzbv_2VnNamEHJXGnThsEJFlmHWrBVpfiQ&s',
+    coverImage: 'https://i.ytimg.com/vi/0jhWgCxbEY8/maxresdefault.jpg',
     bio: 'Echo Valley brings a fresh perspective to indie folk with their harmony-rich arrangements and storytelling approach. The duo met at a local coffee shop\'s open mic night and immediately connected over their shared love of folk music.\n\nTheir acoustic-driven sound and authentic lyrics have earned them spots at major folk festivals across the country. Their latest EP showcases their evolution as artists while staying true to their roots.',
     social: {
-      instagram: 'https://instagram.com/echovalley',
-      twitter: 'https://twitter.com/echovalley',
-      youtube: 'https://youtube.com/echovalley'
+      instagram: 'https://instagram.com/',
+      twitter: 'https://twitter.com/',
+      youtube: 'https://youtube.com/'
     },
-    website: 'https://echovalley.com',
+    website: '',
     releases: [
-      {
-        id: 'mountain-song',
-        title: 'Mountain Song',
-        cover: 'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?auto=format&fit=crop&q=80',
-        year: '2023'
-      }
+      //{
+        // id: 'mountain-song',
+        // title: 'Mountain Song',
+        // cover: 'https://images.unsplash.com/photo-1459749411175-04bf5292ceea?auto=format&fit=crop&q=80',
+        // year: '2023'
+      //}
     ]
   },{
     id: 'cothelo',
     name: 'C.OTHELLO',
     genre: 'RNB POP FUNK ',
-    image: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/455037430_7962997657151342_798543563596905620_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeG5_rmWQYF6L-EnjzDKFNHUfL9QLmWmYPl8v1AuZaZg-RPsyrgBJZaJeZ4qg0XhkklXXZToIJnN9x8OpFYUxblV&_nc_ohc=On-TMvYatFoQ7kNvgGZR6sG&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=AizA7OzgvJNGlGut29M8_aZ&oh=00_AYARLl2V7RZNKKM8sAtv8_jbMZKTL_HXZdwJGyfTLn15PQ&oe=673C93E5',
-    coverImage: 'https://scontent.fcmb1-2.fna.fbcdn.net/v/t51.75761-15/466021815_18293478088229321_4523986414111524634_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFtPpfa7e0GF2j-89yWVzQEAY-AT5EpkgQBj4BPkSmSBJly0TbDz4reVVo271khX25ga6zciT9GaVk4XBYBqAmv&_nc_ohc=RbwNHZ1StWgQ7kNvgGLjJ3Q&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=AO2A101lCi904ofglyl7AsL&oh=00_AYDVl5tunTuNqVc8W7o05MaVFORFTxQmk21_FlBGUS8bAQ&oe=673C9A4F',
+    image: 'https://i.ibb.co/932gQj1/444486253-7519593031491809-5819787794647506851-n.jpg',
+    coverImage: 'https://i.ibb.co/932gQj1/444486253-7519593031491809-5819787794647506851-n.jpg',
     bio: 'Echo Valley brings a fresh perspective to indie folk with their harmony-rich arrangements and storytelling approach. The duo met at a local coffee shop\'s open mic night and immediately connected over their shared love of folk music.\n\nTheir acoustic-driven sound and authentic lyrics have earned them spots at major folk festivals across the country. Their latest EP showcases their evolution as artists while staying true to their roots.',
     social: {
       instagram: 'https://instagram.com/echovalley',
@@ -117,8 +107,8 @@ export const artistsData = [
       }
     ]
   },{
-    id: 'luna-eclipse',
-    name: 'We Are Brothers',
+    id: 'Vishensanajana',
+    name: 'Vishen Sanjana',
     genre: 'Pop , Accoustic',
     spot: 'https://open.spotify.com/embed/artist/5ajmxFKVTfNM0FZYRPCiko?utm_source=generator&theme=0',
     yts:'https://www.youtube.com/embed/0WbwxTb1i8s?si=4_A135D7qmMq3Gkm',
