135ed72ea48a8b7f3ed04b773752dd90268d5c1d
wolferlk <sasiofficial25@gmail.com>
2025-01-10 12:38:19 +0530

    kkkk

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <section id="home" className="relative h-screen flex items-center justify-center">
       <div className="absolute inset-0">
         <img
-          src="https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/463897057_910267737815971_9038813593128774045_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeEL3P2okwqVXqBctBBRKFPp3DW1jBxxaWbcNbWMHHFpZlLpZ7FoxV7cX-pG8EqrJCAWMKeGWW3yRyUgoStq4gBu&_nc_ohc=xYcyp3TUe3wQ7kNvgFksCbc&_nc_zt=23&_nc_ht=scontent.fcmb1-2.fna&_nc_gid=Ay2w_XYYSWcse6ynWIsreUm&oh=00_AYB_78fELbzbrwGmnqS2e-JA0EXsYfwU-Gxcoql_rpPqww&oe=673C7B1C"
+          src="https://videos.pexels.com/video-files/5657831/5657831-uhd_2732_1440_25fps.mp4"
           alt="Studio"
           className="w-full h-full object-cover opacity-50"
         />
